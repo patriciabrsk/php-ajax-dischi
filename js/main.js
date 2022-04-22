673432cbf3a41357997a246af4de45858d6db636
patriciabrsk <patriciabresku@gmail.com>
2022-04-22 18:39:23 +0200

    lol stupid error..

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@ const app = new Vue({
     'el' : '#app',
     data: {
         records: [],
-        created() {
-            axios
-            .get("http://localhost:8888/php-ajax-dischi/server/controller.php")
-            .then((response) => {
-                this.records = response.data.results;
-            }).catch((error) => {
-                console.error(error);
-            });
-        },
+    },
+    created() {
+        axios
+        .get('http://localhost:8888/php-ajax-dischi/server/controller.php')
+        .then((response) => {
+            this.records = response.data.results;
+        }).catch((error) => {
+            console.error(error);
+        });
     },
 });
